1767e0b48ebbc19007dc04086537df3f503adfdc
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2022-12-14 18:04:17 -0300

    fix premium

--- app/Http/Controllers/PremiumController.php
+++ app/Http/Controllers/PremiumController.php
@@ -31,7 +31,7 @@ class PremiumController extends Controller
                                 ->first();
 
 
-
+        $premiumAccount =  cabalChangeAuth::where('UserNum', Auth::user()->UserNum)->first();
         $expiracao  = date('Y-m-d H:i:s.B', strtotime('+30 days'));
 		$MyIPAtual  = $_SERVER["REMOTE_ADDR"];
 		$Web_Date   = date('Y-m-d H:i:s.B');
@@ -47,63 +47,76 @@ class PremiumController extends Controller
         else
         {
 
-            // cash total
-            $valorCash = $cash->Cash - $vipPremium_valor;
+            if($premiumAccount->ServiceKind === 0)
+            {
+                // cash total
+                $valorCash = $cash->Cash - $vipPremium_valor;
 
-            // descont cash vip
-            cabalCash::where('ID', Auth::user()->ID)
-                        ->where('UserNum', Auth::user()->UserNum)
-                        ->update(['Cash' => $valorCash]);
+                // descont cash vip
+                cabalCash::where('ID', Auth::user()->ID)
+                            ->where('UserNum', Auth::user()->UserNum)
+                            ->update(['Cash' => $valorCash]);
 
-            // ADD VIP ACCOUNT
-            cabalChangeAuth::where('UserNum', Auth::user()->UserNum)->delete(); // delete USER VIP
-            cabalChangeAuth::create([
-                'UserNum'       => Auth::user()->UserNum,
-                'ServiceKind'   => $premiumID,
-                'ExpireDate'    => $expiracao
-            ]); // adiciona vip
+                // ADD VIP ACCOUNT
+                cabalChangeAuth::where('UserNum', Auth::user()->UserNum)->delete(); // delete USER VIP
+                cabalChangeAuth::create([
+                    'UserNum'       => Auth::user()->UserNum,
+                    'ServiceKind'   => $premiumID,
+                    'ExpireDate'    => $expiracao
+                ]); // adiciona vip
 
-            // ADD LOGS
-            cabalLogsPremium::create([
-                'Account'   => Auth::user()->ID,
-                'UserNum'   => Auth::user()->UserNum,
-                'IP'        => $MyIPAtual,
-                'Data'      => $Web_Date,
-                'Tipo'      => $premiumID ,
-            ]);
+                // ADD LOGS
+                cabalLogsPremium::create([
+                    'Account'   => Auth::user()->ID,
+                    'UserNum'   => Auth::user()->UserNum,
+                    'IP'        => $MyIPAtual,
+                    'Data'      => $Web_Date,
+                    'Tipo'      => $premiumID ,
+                ]);
 
-            // ITEMS ENVIADOS
-            DB::update( // POCAO INESGOTAVEL 1K HP
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '3182', '0', '17' ")
-            );
-            DB::update( // POCAP INESGOTAVEL 1K MP
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '3183', '0', '17' ")
-            );
-            DB::update( // DESTREINAMENTO DE PET
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '7433', '30', '31' ")
-            );
-            DB::update( // CARTAO LOJA REMOTA
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '1275', '0', '17' ")
-            );
-            DB::update( // CARTAO ARMAGEM REMOTO
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '2335', '0', '17' ")
-            );
-            DB::update( // CARTAO AGENTE REMOTO
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '1542', '0', '17' ")
-            );
-            DB::update( // NUCLEO GUIA
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '944', '0', '17' ")
-            );
-            DB::update( // PEDRA DA FRONTEIRA 3
-                DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '6433', '0', '17' ")
-            );
+                // ITEMS ENVIADOS
+                DB::update( // POCAO INESGOTAVEL 1K HP
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '7278', '0', '17' ")
+                );
+                DB::update( // POCAO INESGOTAVEL 1K MP
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '7279', '0', '17' ")
+                );
+                DB::update( // DESTREINAMENTO DE PET
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '7433', '10', '31' ")
+                );
+                DB::update( // CARTAO LOJA REMOTA
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '1275', '0', '17' ")
+                );
+                DB::update( // CARTAO ARMAGEM REMOTO
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '2335', '0', '17' ")
+                );
+                DB::update( // CARTAO AGENTE REMOTO
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '1542', '0', '17' ")
+                );
+                DB::update( // NUCLEO GUIA
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '5040', '0', '17' ")
+                );
+                DB::update( // PEDRA DA FRONTEIRA 3
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '6433', '0', '17' ")
+                );
+                DB::update( // PEDRA DA FRONTEIRA 3
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '6433', '0', '17' ")
+                );
+                DB::update( // PEDRA DA FRONTEIRA 3
+                    DB::raw("USE [CabalCash]exec  up_AddMyCashItemByItem '".Auth::user()->UserNum."', '1', '1', '6433', '0', '17' ")
+                );
 
-            return response()->json([
-                'success' => true,
-                'message' => "Obrigado por efetuar a compra do VIP no Cabal Hype, Seu vip já foi ativado",
-            ]);
-
-
+                return response()->json([
+                    'success' => true,
+                    'message' => "Obrigado por efetuar a compra do VIP no Cabal Hype, Seu vip já foi ativado",
+                ]);
+            }
+            else{
+                return response()->json([
+                    'success' => false,
+                    'message' => "Você deve esperar seu vip vencer para poder renovado!",
+                ]);
+            }
         }
 
     }
--- app/Models/Transations.php
+++ app/Models/Transations.php
@@ -14,6 +14,15 @@ class Transations extends Model
 
     protected $table = 'strdeveloped_transations';
 
+	protected $fillable = [
+		'id_user',
+		'id_payment',
+		'id_ref',
+		'id_pacote',
+		'status',
+		'created_at'
+	];
+	
     public function getAccount()
     {
         return $this->belongsTo(cabalAuth::class, 'id_user', 'UserNum');
